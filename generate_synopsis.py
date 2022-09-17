630d820538ee0c4d3d0af5674400d208dbec2a84
David Shapiro <daveshap37@gmail.com>
2022-09-17 14:35:47 -0400

    all done

--- generate_synopsis.py
+++ generate_synopsis.py
@@ -19,7 +19,7 @@ def save_file(filepath, content):
 openai.api_key = open_file('openaiapikey.txt')
 
 
-def gpt3_completion(prompt, engine='text-davinci-002', temp=1.1, top_p=1.0, tokens=1000, freq_pen=0.0, pres_pen=0.0, stop=['asdfasdf', 'asdasdf']):
+def gpt3_completion(prompt, engine='text-davinci-002', temp=1.1, top_p=1.0, tokens=3000, freq_pen=0.0, pres_pen=0.0, stop=['asdfasdf', 'asdasdf']):
     max_retry = 5
     retry = 0
     prompt = prompt.encode(encoding='ASCII',errors='ignore').decode()  # force it to fix any unicode errors
